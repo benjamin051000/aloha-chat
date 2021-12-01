e1f1d23a2b98d6c217dc4e549cdff5639f077c12
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2021-12-01 15:44:11 -0500

    moved files into /client and /server subdirs

--- src/client/ReadThread.java
+++ src/client/ReadThread.java
@@ -1,3 +1,4 @@
+package client;
 // package net.codejava.networking.chat.client;
  
 import java.io.*;
--- src/server/UserThread.java
+++ src/server/UserThread.java
@@ -1,3 +1,4 @@
+package server;
 // package net.codejava.networking.chat.server;
  
 import java.io.*;
