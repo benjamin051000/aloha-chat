6445a9effd2c78ccd90773dac5628e4f7c6853ef
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2021-12-01 15:37:30 -0500

    grammar police

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -77,7 +77,7 @@ public class ChatServer {
         boolean removed = userNames.remove(userName);
         if (removed) {
             userThreads.remove(aUser);
-            System.out.println("The user " + userName + " quitted");
+            System.out.println(userName + " left");
         }
     }
  
--- src/UserThread.java
+++ src/UserThread.java
@@ -48,7 +48,7 @@ public class UserThread extends Thread {
             server.removeUser(userName, this);
             socket.close();
  
-            serverMessage = userName + " has quitted.";
+            serverMessage = userName + " has quit.";
             server.broadcast(serverMessage, this);
  
         } catch (IOException ex) {
